3d732bbbf5d7899552959d7a9e8a3ad0c6569b21
Alois Roscher <aloisroscher@gmail.com>
2021-11-14 16:06:29 +0100

    added Leftsidemenu, added icons-material

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,27 @@
+// @flow
+import { Typography } from "@mui/material";
+import * as React from "react";
+import logo from "../logo.svg";
+
+type Props = {};
+export function Homepage(props: Props) {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
