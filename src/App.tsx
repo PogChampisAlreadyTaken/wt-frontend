a05cd2125172a8b46e71a48fd4437a84165e47ae
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2021-12-08 02:01:13 +0100

    added login page, cat animation and demo mode for the login situation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import "./App.css";
 import PageWrapper from "./components/PageWrapper";
 import { Overview } from "./pages/Overview";
 import { History } from "./pages/History";
 import { Exchange } from "./pages/Exchange";
 import { Explore } from "./pages/Explore";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route
+            path="/"
+            element={
+              <PageWrapper>
+                <Login />
+              </PageWrapper>
+            }
+          />
           <Route
             path="/overview"
             element={
---
+++ src/pages/Login.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import SendIcon from "@mui/icons-material/Send";
+import Lottie from "react-lottie";
+import animationData from "./login.json";
+
+export function Login() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+
+            style={{backgroundColor: "#6157f4"}}          
+            variant="contained"
+            fullWidth
+            endIcon={<SendIcon style={{ fill: "#6157f4" }} />}
+            color="primary"
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
