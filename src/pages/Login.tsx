025a7066f947dea4aecf26a5ab133eaef4184990
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2021-12-08 16:20:14 +0100

    changed animations position

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import SendIcon from "@mui/icons-material/Send";
 import Lottie from "react-lottie";
-import animationData from "./login.json";
+import animationData from "../components/animations/login.json";
 
 export function Login() {
   const defaultOptions = {
@@ -53,8 +51,8 @@ export function Login() {
             id="email"
             label="Email Address"
             name="email"
-            autoComplete="email"
             autoFocus
+            autoComplete="new-password"
           />
           <TextField
             margin="normal"
@@ -64,11 +62,10 @@ export function Login() {
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
+            autoComplete="new-password"
           />
           <Button
-
-            style={{backgroundColor: "#6157f4"}}          
+            style={{ backgroundColor: "#6157f4" }}
             variant="contained"
             fullWidth
             endIcon={<SendIcon style={{ fill: "#6157f4" }} />}
