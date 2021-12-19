16d23a7298cd0f524de68569a41ef49ef6fd04e8
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2021-12-20 00:34:10 +0100

    add br

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -16,7 +16,6 @@ const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
-
 ];
 
 export function Explore() {
