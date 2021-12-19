dc9149cb95450ef437efa163a2b98ae62ab5bb46
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2021-12-19 13:52:42 +0100

    testing data models and table

---
+++ src/model/coin.tsx
@@ -0,0 +1,12 @@
+interface Coin {
+  id: string;
+  name: string;
+  market_data: MarketData;
+}
+
+export interface MarketData {
+  current_price: { [key: string]: number };
+  market_cap: { [key: string]: number };
+  market_cap_rank: number;
+  price_change_percentage_7d: number;
+}
--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,7 +1,63 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { makeStyles } from "@material-ui/core/styles";
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+
+];
+
 export function Explore() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className={classes.exploreTable}>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Coin</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">24h</TableCell>
+            <TableCell align="right">Last 7 Days</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 }
+
+
+const useStyles = makeStyles({
+  exploreTable: {
+    padding: "5px"
+  },
+});
